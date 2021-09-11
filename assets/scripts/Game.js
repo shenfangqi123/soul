4fd18230f113b0696d13eda65ededaa6133bf197
shen fangqi <shenfq@weic.jp>
2021-09-11 17:07:34 +0900

    init

--- assets/scripts/Game.js
+++ assets/scripts/Game.js
@@ -9,36 +9,91 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        // 这个属性引用了预制资源
+        playerPrefab: {
+            default: [],
+            type: cc.Prefab
+        },
+        audios: {
+            default: [],
+            type: cc.AudioClip
+        }
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
         cc.director.getPhysicsManager().enabled = true;
-        cc.director.getPhysicsManager().debugDrawFlags = cc.PhysicsManager.DrawBits.e_aabbBit ||cc.PhysicsManager.DrawBits.e_pairBit;
+        //cc.director.getPhysicsManager().debugDrawFlags = cc.PhysicsManager.DrawBits.e_aabbBit ||cc.PhysicsManager.DrawBits.e_pairBit;
 
         cc.director.getCollisionManager().enabled = true;
-        cc.director.getCollisionManager().enabledDebugDraw = true; //开启碰撞检测范围的绘制
-        cc.director.getCollisionManager().enabledDrawBoundingBox = true; //开启碰撞组件的包围盒绘制        
+        //cc.director.getCollisionManager().enabledDebugDraw = true; //开启碰撞检测范围的绘制
+        //cc.director.getCollisionManager().enabledDrawBoundingBox = true; //开启碰撞组件的包围盒绘制        
+
+
+
+
+
+        this._material = this.node.getComponent(cc.RenderComponent).getMaterial(0);  
+
+        let sprite = this.node.getComponent(cc.Sprite);
+        let spriteFrame = sprite.spriteFrame;
+ 
+        let texture = spriteFrame.getTexture();
+        let textureWidth = texture.width;
+        let textureHeight = texture.height;
+
+        console.log("width is ",textureWidth," and height is ",textureHeight);
+
+        //this.node.on(cc.Node.EventType.TOUCH_MOVE,this.move,this);
+        console.log("width is ",this.node.width," and height is ",this.node.height);
+
+        //this._material.setProperty('width',this.node.width);
+        //this._material.setProperty('height',this.node.height);
+        //this._material.setProperty('xx',0.5);
     },
 
-    start () {
+    start () { 
+        this.getHero();
+        //this.shadowForAgent();
     },
+
+    getHero: function() {
+        var hero = this.node.getChildByName("hero");
+        heroOp = hero.getComponent("playerScript");
+        heroOp.setShadow(this.shadowForAgent());
+    },
+
+    shadowForAgent: function () {
+        var shadowObj = cc.instantiate(this.playerPrefab[0]);
+        shadowObj.active = true;
+        this.node.addChild(shadowObj);
+
+        shadowObj.x =0;
+        shadowObj.y =0;
+        shadowObj.zIndex =-1;
+
+        return shadowObj;
+    },
+
+
+/*
+    move(event) {
+        let ratio = this.node.height / this.node.width;
+ 
+        let location = event.getLocation();
+        console.log("location.y is ",location.y);
+        let x = location.x / cc.winSize.width;
+        let y = (location.y / cc.winSize.height);
+ 
+        console.log("x is ",x);
+        console.log("y is ",y);
+ 
+        let center = cc.v2(x,1 - y);
+        this._material.setProperty('center',center);
+    }
+*/
+     
 
     // update (dt) {},
 });
--- assets/scripts/playerScript.js
+++ assets/scripts/playerScript.js
@@ -19,14 +19,24 @@ cc.Class({
             default:null,
         },
 
-        speed:100,
+        speed:10,
+        shadow:cc.NOde,
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad () {
+        this.darkMask = this.node.parent.getChildByName("darkMask");
+        this._material = this.darkMask.getComponent(cc.RenderComponent).getMaterial(0);
+        cc.log(this._material);
+        this._material.setProperty('center', cc.v2(0.5,0.5));
+    },
 
     start () {
+    },
+
+    setShadow: function (shadow) {
+        this.shadow = shadow;
     },
 
     update (dt) {
@@ -43,6 +53,17 @@ cc.Class({
         this.node.x += sx;
         this.node.y += sy;
 
+        this.shadow.x = this.node.x;
+        this.shadow.y = this.node.y;
+
+        var maskX = this.node.x + 2500;
+        var maskY = this.node.y + 2500;
+
+        var ratioX = maskX / 5000;
+        var ratioY = 1- (maskY / 5000);
+
+        this._material.setProperty('center', cc.v2(ratioX, ratioY));
+
         //this.MainCamera.x += sx;
         //this.MainCamera.y += sy;
 
@@ -54,5 +75,136 @@ cc.Class({
         var degree = r * 180/(Math.PI);
         //degree = 360 - degree + 90;
         this.node.angle = degree + 270;
+        this.shadow.angle = this.node.angle;
+
+
     },
+
+    s1: function () {
+        this.dispShadow(0);
+    },
+
+    s2: function () {
+        this.dispShadow(1);
+    },
+
+    s3: function () {
+        this.dispShadow(2);
+    },
+
+    s4: function () {
+        this.dispShadow(3);
+    },
+
+    s5: function () {
+        this.dispShadow(4);
+    },
+
+    s6: function () {
+        this.dispShadow(5);
+    },
+
+    s7: function () {
+        this.dispShadow(6);
+    },
+
+    s8: function () {
+        this.dispShadow(7);
+    },
+
+    s9: function () {
+        this.dispShadow(8);
+    },
+
+    s10: function () {
+        this.dispShadow(9);
+    },
+
+    s11: function () {
+        this.dispShadow(10);
+    },
+
+    s12: function () {
+        this.dispShadow(11);
+    },
+
+    s13: function () {
+        this.dispShadow(12);
+    },
+
+    s14: function () {
+        this.dispShadow(13);
+    },
+
+    s15: function () {
+        this.dispShadow(14);
+    },
+
+    s16: function () {
+        this.dispShadow(15);
+    },
+
+    s17: function () {
+        this.dispShadow(16);
+    },
+
+    s18: function () {
+        this.dispShadow(17);
+    },
+
+    s19: function () {
+        this.dispShadow(18);
+    },
+
+    dispShadow: function(frameNo) {
+        //shadow object may not ready in init() when playing
+        if(!this.shadow) return;
+
+        var shadowNode = this.shadow;
+        var frameImg = "actors/hero/n/shadow/"+frameNo;
+        var act = "n";
+
+        if(!act) return;
+
+/*
+        var actTmp = this.lastAct.split("_");
+        var actDir = actTmp[1];
+        var actType = actTmp[2];
+        var scaleX = this.lastScaleX;
+*/
+
+        actDir = act;
+        if(actDir == "en1" || actDir == "en2" || actDir == "en3") {
+            frameImg = "actors/hero/n/shadow/"+frameNo;
+        }
+        else if(actDir == "se1" || actDir == "se2" || actDir == "se3") {
+            frameImg = "actors/hero/n/shadow/"+frameNo;
+        }
+        else if(actDir == "s") {
+            frameImg = "actors/hero/n/shadow/"+frameNo;
+        }
+        else if(actDir == "n") {
+            frameImg = "actors/hero/n/shadow/"+frameNo;
+        }
+        else if(actDir == "e") {
+            frameImg = "actors/hero/n/shadow/"+frameNo;
+        }
+
+        this.shadow.active = true;
+
+        cc.loader.loadRes(frameImg, cc.SpriteFrame, function (err, spriteFrame) {
+            if(shadowNode) {
+                try {
+                    if(shadowNode._name != "") {
+                        shadowNode.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+                    }
+                } catch (e) {
+                    console.log(shadowNode);
+                    console.log(e);
+                }
+            }
+        });
+
+    },
+
 });
